b5a3a64afd031be08f035ba4476428f18e202722
Tanzim <tanzim008@gmail.com>
2024-07-25 01:18:36 -0230

    Created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Cart from './pages/Cart'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
-
